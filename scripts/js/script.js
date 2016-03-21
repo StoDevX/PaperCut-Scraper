1a216e67ded2f7173c8d11682d9f5044b0d030a8
peters10 <peters10@stolaf.edu>
2016-03-20 23:16:02 -0500

    reformatt all data, one chart

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -1,94 +1,142 @@
-var margin = {top:20, right:30, bottom:30, left:-20},
-    width = 11960-margin.left - margin.right,
-    height = 1500-margin.top-margin.bottom;
+var newData = []
+var max10 = []
+var max_value = 0;
+d3.json("data/data.json", function(data){
+    for(var i=0;i<data.length;i++){
+	if(data[i] != null) {
+	    newData.push(data[i]);
+	}
+    }
 
-// scale to ordinal because x axis is not numerical
-var x = d3.scale.ordinal().rangeRoundBands([0, 150000], .3);
+    newData.sort(function(a, b) {
+	return b.month-a.month
+    });
+    
+    max_value = newData[0].month;
+    console.log(newData.length);
+    
+    var margin = { top: 30, right: 30, bottom: 40, left: 50}
 
-//scale to numerical value by height
-var y = d3.scale.linear().range([height, 1200]);
+    var height = 400 - margin.top - margin.bottom,
+	width = 20000 - margin.left - margin.right,
+	barWidth = 100,
+	barOffset = 5;
 
-var chart = d3.select("#chart")
-              .append("svg")  //append svg element inside #chart
-              .attr("width", width+(2*margin.left)+margin.right)    //set width
-              .attr("height", height+margin.top+margin.bottom);  //set height
+    var tempColor;
 
-var xAxis = d3.svg.axis()
-              .scale(x)
-              .orient("bottom");  //orient bottom because x-axis will appear below the bars
+    var colors = d3.scale.category10();
 
-var yAxis = d3.svg.axis()
-              .scale(y)
-              .orient("left");
+    var yScale = d3.scale.linear()
+        .domain([0, max_value])
+        .range([0, height])
+
+    var xScale = d3.scale.ordinal()
+	.domain(d3.range(0, newData.length))
+        .rangeBands([0, width])
+
+    var tooltip = d3.select('body').append('div')
+        .style('position', 'absolute')
+        .style('padding', '0 10px')
+        .style('background', 'white')
+        .style('opacity', 0)
+    
+    var myChart = d3.select('#chart1').append('svg')
+        .style('background', '#eee8d5')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left +', '+ margin.top +')')
+        .selectAll('rect').data(newData)
+        .enter().append('rect')
+        .style('fill', function(d,i) {
+	    return colors(i);
+	})
+        .attr('width', xScale.rangeBand())
+        .attr('height', function(d) {
+	    return yScale(d.month);
+	})
+        .attr('x', function(d,i) {
+	    return xScale(i);
+	})
+        .attr('y', function(d) {
+	    return height - yScale(d.month);
+	})
+        .on('mouseover', function(d) {
+
+            tooltip.transition()
+		.style('opacity', .9)
+
+            tooltip.html(d.login+ ": month: " + d.month+ ", week: " + d.week)
+		.style('left', (d3.event.pageX - 65) + 'px')
+		.style('top',  (d3.event.pageY - 60) + 'px')
 
 
+            tempColor = this.style.fill;
+            d3.select(this)
+		.style('opacity', .5)
+		.style('fill', 'yellow')
+	})
 
-d3.json("data/data.json", function(error, data){
-  //Remove null data (in case some incorrect usernames were entered)
-  var newData = []
-  for(var i=0;i<data.length;i++){
-    if(data[i] != null) newData.push(data[i]);
-  }
-  data = newData;
-  // Sort the data
-  data = data.sort( function(a,b) {return b.month-a.month;});
+	.on('mouseout', function(d) {
+            d3.select(this)
+		.style('opacity', 1)
+		.style('fill', tempColor)
+	})
 
-    x.domain(data.map(function(d){return d.login}));
-    y.domain([0, d3.max(data, function(d){return d.month})]);
+     var vGuideScale = d3.scale.linear()
+        .domain([0, max_value])
+        .range([height, 0])
 
-    var bar = chart.selectAll("g")
-                      .data(data)
-                    .enter()
-                      .append("g")
-                      .attr("transform", function(d, i){
-                        return "translate("+x(d.login)+", 0)";
-                      });
+    var vAxis = d3.svg.axis()
+        .scale(vGuideScale)
+        .orient('left')
+        .ticks(10)
 
-    bar.append("rect")
-        .attr("y", function(d) {
-          return y(d.month);
-        })
-        .attr("x", function(d,i){
-          return x.rangeBand()+(margin.left/1);
-        })
-        .attr("height", function(d) {
-          return height - y(d.month);
-        })
-        .attr("width", x.rangeBand());  //set width base on range on ordinal data
+    var vGuide = d3.select('svg').append('g')
+        vAxis(vGuide)
+        vGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+        vGuide.selectAll('path')
+            .style({ fill: 'none', stroke: "#000"})
+        vGuide.selectAll('line')
+            .style({ stroke: "#000"})
 
-    bar.append("text")
-        .attr("x", x.rangeBand()+ 0 )
-        .attr("y", function(d) { return y(d.month) -10; })
-        .attr("dy", ".75em")
-        .text(function(d) { return d.month; });
+    var hAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('bottom')
+        .tickFormat('')
 
-    chart.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate("+margin.left+","+ height+")")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis)
-          .selectAll("text")
-              .style("text-anchor", "end")
-              .attr("dx", "-.8em")
-              .attr("dy", ".15em")
-              .attr("transform", function(d) {
-                  return "rotate(-65)"
-                  });
+    var hGuide = d3.select('svg').append('g')
+        hAxis(hGuide)
+        hGuide.attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')')
+        hGuide.selectAll('path')
+            .style({ fill: 'none', stroke: "#000"})
+        hGuide.selectAll('line')
+            .style({ stroke: "#000"})
+ 
 
-    chart.append("g")
-          .attr("class", "y axis")
-          .attr("transform", "translate("+margin.left+",0)")
-          .call(yAxis)
-          .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("Month");
 
-  });
+    /*var myChart = d3.select('#chart2').append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .style('background', '#C9D7D6')
+        .selectAll('rect').data(max10)
+        .enter().append('rect')
+        .style('fill', '#C61C6F')
+        .attr('width', xScale.rangeBand())
+        .attr('height', function(d) {
+	    return yScale(d);
+	})
+        .attr('x', function(d,i) {
+	    return xScale(i);
+	})
+        .attr('y', function(d) {
+	    return height - yScale(d);
+	})
+        .on('mouseover', function(d) {
+	    d3.select(this)
+	        .style('opacity', .5)
+	})*/
 
-function type(d) {
-    d.login = +d.login; // coerce to number
-    return d;
-  }
+    
+    
+});
